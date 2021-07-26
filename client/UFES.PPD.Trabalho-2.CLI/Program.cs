c337ae64adaed000295b577ff72aa6630d3259a8
Felipe Endlich <endlichfelipe@gmail.com>
2021-07-26 19:28:21 -0300

    proto definition

---
+++ client/UFES.PPD.Trabalho-2.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace UFES.PPD.Trabalho_2.CLI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+        }
+    }
+}
